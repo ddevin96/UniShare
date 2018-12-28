7df2a601aed927027f34dff78797168e80100c05
ddevin96 <danieledevinco1996@gmail.com>
2018-12-28 17:09:16 +0100

    Classe Bacheca

---
+++ app/src/main/java/com/example/dani/unishare/Bacheca.java
@@ -0,0 +1,64 @@
+package com.example.dani.unishare;
+
+import java.util.Date;
+
+public class Bacheca {
+    private String id;
+    private String title;
+    private String description;
+    private String author; //tipo utente???
+    // array di post
+    private Date data;
+
+    public Bacheca() {
+
+    }
+
+    public Bacheca(String id, String title, String description, String author, Date data) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}

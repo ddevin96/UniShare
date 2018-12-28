30294dd2560428d6d3aebd4a827825e32e98d3bf
ddevin96 <danieledevinco1996@gmail.com>
2018-12-28 18:11:01 +0100

    classi bacheca

--- app/src/main/java/com/example/dani/unishare/MainActivity.java
+++ app/src/main/java/com/example/dani/unishare/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 public class MainActivity extends Activity {

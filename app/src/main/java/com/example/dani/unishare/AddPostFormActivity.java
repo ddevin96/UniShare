03ea0a9aba7f87cec41cf58b4643ca5566b4db5c
ddevin96 <danieledevinco1996@gmail.com>
2018-12-29 19:03:49 +0100

    compact update

--- app/src/main/java/com/example/dani/unishare/AddPostFormActivity.java
+++ app/src/main/java/com/example/dani/unishare/AddPostFormActivity.java
@@ -1,8 +1,7 @@
 package com.example.dani.unishare;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -21,7 +20,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class AddPostFormActivity extends AppCompatActivity {
+public class AddPostFormActivity extends Activity {
 
     EditText editTextTitle;
     EditText editTextDescription;

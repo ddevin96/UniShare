d9817d70fa6398747d4a778068db069899e9eb4f
ddevin96 <danieledevinco1996@gmail.com>
2018-12-29 18:16:48 +0100

    manifest update

--- app/src/main/java/com/example/dani/unishare/AddPostFormActivity.java
+++ app/src/main/java/com/example/dani/unishare/AddPostFormActivity.java
@@ -1,9 +1,9 @@
 package com.example.dani.unishare;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class AddPostFormActivity extends AppCompatActivity {
+public class AddPostFormActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

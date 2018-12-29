3257cf2c40ef12573b3ea9a56d0913bd79a02c73
ddevin96 <danieledevinco1996@gmail.com>
2018-12-29 17:12:44 +0100

    commentlist + xml

---
+++ app/src/main/java/com/example/dani/unishare/CommentiList.java
@@ -0,0 +1,37 @@
+package com.example.dani.unishare;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CommentiList extends ArrayAdapter<Commento> {
+    Activity context;
+    List<Commento> lista;
+
+    public CommentiList (Activity context, List<Commento> lista) {
+        super(context, R.layout.list_view_commenti, lista);
+        this.context = context;
+        this.lista = lista;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.list_view_commenti,null, true );
+
+        TextView textViewCommentDescription = (TextView) listViewItem.findViewById(R.id.textViewCommentDescription);
+        TextView textViewCommentAuthor = (TextView) listViewItem.findViewById(R.id.textViewCommentAuthor);
+
+        Commento commento = lista.get(position);
+        textViewCommentDescription.setText(commento.getDescription());
+        textViewCommentAuthor.setText(commento.getAuthor());
+
+        return listViewItem;
+    }
+}

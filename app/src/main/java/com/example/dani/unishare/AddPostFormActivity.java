e67bfaed95251ac34c002555ab8ae9d7b93bcf18
Dona9 <dona.9724@gmail.com>
2018-12-29 18:50:36 +0100

    post

--- app/src/main/java/com/example/dani/unishare/AddPostFormActivity.java
+++ app/src/main/java/com/example/dani/unishare/AddPostFormActivity.java
@@ -1,13 +1,71 @@
 package com.example.dani.unishare;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class AddPostFormActivity extends AppCompatActivity {
+
+    EditText editTextTitle;
+    EditText editTextDescrizione;
+    Button pubblica;
+    DatabaseReference databasePost;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post_form);
+
+        databasePost = FirebaseDatabase.getInstance().getReference("post");
+
+        editTextTitle = (EditText) this.findViewById(R.id.titlePost);
+        editTextDescrizione = (EditText) this.findViewById(R.id.descrizionePost);
+        pubblica = (Button) this.findViewById(R.id.addPostButton);
+
+
+        pubblica.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addPost();
+            }
+
+            Intent intent = new Intent(getApplicationContext(), PostActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+    public void addPost() {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescrizione.getText().toString();
+
+
+        if (!TextUtils.isEmpty(title)&&!TextUtils.isEmpty(description)) {
+            String id = databasePost.push().getKey();
+            Post post = new Post(id, title, description, author, data);
+            databasePost.child(id).setValue(post);
+            Toast.makeText(this, "Post aggiunto", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(this, "Inserisci titolo e descrizione", Toast.LENGTH_SHORT).show();
+
+
+
     }
 }

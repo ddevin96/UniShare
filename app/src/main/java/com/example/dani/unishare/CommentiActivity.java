583d7e7271738f747dff914401334dfb666908ea
ddevin96 <danieledevinco1996@gmail.com>
2018-12-29 16:45:08 +0100

    comment activity + xml

--- app/src/main/java/com/example/dani/unishare/CommentiActivity.java
+++ app/src/main/java/com/example/dani/unishare/CommentiActivity.java
@@ -1,13 +1,97 @@
 package com.example.dani.unishare;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class CommentiActivity extends Activity {
+
+    TextView textViewPostName;
+    EditText editTextCommentDescription;
+    Button addCommentButton;
+    ListView listViewCommenti;
+    DatabaseReference databaseCommenti;
+    List<Commento> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commenti);
+
+        textViewPostName = (TextView) findViewById(R.id.textViewPostName);
+        editTextCommentDescription = (EditText) findViewById(R.id.editTextCommentDescription);
+        addCommentButton = (Button) findViewById(R.id.addCommentButton);
+        listViewCommenti = (ListView) findViewById(R.id.listViewCommenti);
+
+        Intent intent = getIntent();
+
+        lista = new ArrayList<>();
+
+        String id = intent.getStringExtra(PostActivity.POST_ID);
+        String title = intent.getStringExtra(PostActivity.POST_TITLE);
+        textViewPostName.setText(title);
+
+        databaseCommenti = FirebaseDatabase.getInstance().getReference("commento").child(id);
+        addCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addCommento();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        databaseCommenti.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                lista.clear();
+                for (DataSnapshot personSnapshot: dataSnapshot.getChildren()) {
+                    Commento commento = personSnapshot.getValue(Commento.class);
+                    lista.add(commento);
+                }
+
+                CommentiList adapter = new CommentiList(CommentiActivity.this, lista);
+                listViewCommenti.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addCommento() {
+        String description = editTextCommentDescription.getText().toString();
+        String author = "Daniele";
+        Date date = new Date();
+
+        if (!TextUtils.isEmpty(description)) {
+            String id = databaseCommenti.push().getKey();
+            Commento comment = new Commento(id, description, author, date);
+            databaseCommenti.child(id).setValue(comment);
+            Toast.makeText(this, "Commento Inserito", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(this, "Inserisci descrizione", Toast.LENGTH_SHORT).show();
     }
 }

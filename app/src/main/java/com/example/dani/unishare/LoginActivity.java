9ab113600cd72922191816377f83c36272ba1808
Dona9 <dona.9724@gmail.com>
2018-12-31 00:07:22 +0100

    post

--- app/src/main/java/com/example/dani/unishare/LoginActivity.java
+++ app/src/main/java/com/example/dani/unishare/LoginActivity.java
@@ -2,12 +2,29 @@ package com.example.dani.unishare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button accedi;
+    FirebaseAuth databaseLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email= (EditText) findViewById(R.id.InserisciEmail);
+        password= (EditText) findViewById(R.id.InserisciPassword);
+        accedi= (Button) findViewById(R.id.Accedi);
+
+        databaseLogin=
+
+
     }
 }

36f7e435bf334f6a57ce3c231a272fe1f3a946ce
Dona9 <dona.9724@gmail.com>
2019-01-01 21:14:33 +0100

    post

---
+++ app/src/main/java/com/example/dani/unishare/ProfiloActivity.java
@@ -0,0 +1,13 @@
+package com.example.dani.unishare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfiloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilo);
+    }
+}

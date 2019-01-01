dba6db25e434ef4800136cbdc64c01831bdbd598
Dona9 <dona.9724@gmail.com>
2019-01-01 21:39:47 +0100

    post

--- app/src/main/java/com/example/dani/unishare/ProfiloActivity.java
+++ app/src/main/java/com/example/dani/unishare/ProfiloActivity.java
@@ -2,12 +2,55 @@ package com.example.dani.unishare;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfiloActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        TextView textViewNome;
+        TextView textViewCognome;
+        TextView textViewEmail;
+        TextView textViewSesso;
+        TextView textViewData;
+        DatabaseReference databesaProfilo;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profilo);
+
+        textViewNome= (TextView) findViewById(R.id.textViewNomeUser);
+        textViewCognome= (TextView) findViewById(R.id.textViewCognomeUtente);
+        textViewEmail= (TextView) findViewById(R.id.textViewEmailUtente);
+        textViewSesso= (TextView) findViewById(R.id.textViewSessoUtente);
+        textViewData= (TextView) findViewById(R.id.textViewDataUtente);
+
+        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+        databesaProfilo= FirebaseDatabase.getInstance().getReference("utente").child(user.getUid());
+        Utente utente= new Utente();
+
+        databesaProfilo.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot data: dataSnapshot.getChildren()){
+                    data.getValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }

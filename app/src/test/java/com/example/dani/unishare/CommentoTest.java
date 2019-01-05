c6d6e1eb42ca6261f8789b21ac7efb3a048b2ae5
Ventral <luigi.di.palma97@gmail.com>
2019-01-05 11:59:28 +0100

    Testing classe commento

---
+++ app/src/test/java/com/example/dani/unishare/CommentoTest.java
@@ -0,0 +1,48 @@
+package com.example.dani.unishare;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommentoTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+
+    @Test
+    public void getAuthor() {
+    }
+
+    @Test
+    public void setAuthor() {
+    }
+
+    @Test
+    public void getData() {
+    }
+
+    @Test
+    public void setData() {
+    }
+
+    @Test
+    public void getAuthorId() {
+    }
+
+    @Test
+    public void setAuthorId() {
+    }
+}

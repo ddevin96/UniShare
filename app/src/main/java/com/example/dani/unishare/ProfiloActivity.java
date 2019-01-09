589ee14fb7039d14d4e88dba91a1136c27960978
Dona9 <dona.9724@gmail.com>
2019-01-09 17:13:59 +0100

    post

--- app/src/main/java/com/example/dani/unishare/ProfiloActivity.java
+++ app/src/main/java/com/example/dani/unishare/ProfiloActivity.java
@@ -5,9 +5,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -19,17 +17,17 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -71,9 +69,9 @@ public class ProfiloActivity extends Activity {
 
         user = databaseId.getInstance().getCurrentUser();
         databesaProfilo= FirebaseDatabase.getInstance().getReference("utente").child(user.getUid());
-        //databaseBacheche= FirebaseDatabase.getInstance().getReference("bacheca");
-        //databasePost= FirebaseDatabase.getInstance().getReference("post");
-        //databaseCommento= FirebaseDatabase.getInstance().getReference("commento");
+        databaseBacheche= FirebaseDatabase.getInstance().getReference("bacheca");
+        databasePost= FirebaseDatabase.getInstance().getReference("post");
+        databaseCommento= FirebaseDatabase.getInstance().getReference("commento");
 
         listaBacheche = new ArrayList<>();
         listaPost= new ArrayList<>();
@@ -124,7 +122,7 @@ public class ProfiloActivity extends Activity {
 
             }
         });
-       /* databaseBacheche.addValueEventListener(new ValueEventListener() {
+        databaseBacheche.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for(DataSnapshot bachecaSnapshot : dataSnapshot.getChildren()){
@@ -137,8 +135,8 @@ public class ProfiloActivity extends Activity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });*/
-     /*   databasePost.addValueEventListener(new ValueEventListener() {
+        });
+       databasePost.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
@@ -166,7 +164,7 @@ public class ProfiloActivity extends Activity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });*/
+        });
     }
 
     private void modificaProfiloDialog(){
@@ -243,7 +241,15 @@ public class ProfiloActivity extends Activity {
                     editTextPassword.requestFocus();
                     return;
                 }
-                    Date date = new Date(year, month, day);
+                    String d= day + "/" + month + "/" + "/" + year;
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                Date date= null;
+                try {
+                    date = formatter.parse(d);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                //Date date = new Date(year, month, day);
                     String id = user.getUid();
                     user.updateEmail(email);
                     user.updatePassword(password);
@@ -258,6 +264,8 @@ public class ProfiloActivity extends Activity {
             }
         });
     }
+
+
 
     private void deleteProfilo() {
         FirebaseAuth.getInstance().signOut();

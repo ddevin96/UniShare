fd26229d65810194ca8d1fd5b90ef63e42e8d4cb
ddevin96 <danieledevinco1996@gmail.com>
2019-01-09 17:01:00 +0100

    controllo mail in registrazione

--- app/src/main/java/com/example/dani/unishare/RegistrazioneActivity.java
+++ app/src/main/java/com/example/dani/unishare/RegistrazioneActivity.java
@@ -22,10 +22,15 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,6 +45,7 @@ public class RegistrazioneActivity extends Activity {
     Button buttonRegistrazione;
     private FirebaseAuth firebaseAuth;
     DatabaseReference databaseUtente;
+    List<Utente> listaUtente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +64,23 @@ public class RegistrazioneActivity extends Activity {
         radioUomo = (RadioButton) findViewById(R.id.radioUomo);
         buttonRegistrazione = (Button) findViewById(R.id.buttonRegistrazione);
 
+        listaUtente = new ArrayList<>();
         databaseUtente = FirebaseDatabase.getInstance().getReference("utente");
+        databaseUtente.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaUtente.clear();
+                for(DataSnapshot utenteSnapshot: dataSnapshot.getChildren()) {
+                    Utente utente = utenteSnapshot.getValue(Utente.class);
+                    listaUtente.add(utente);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -96,6 +118,12 @@ public class RegistrazioneActivity extends Activity {
         if(TextUtils.isEmpty(cognome)||cognome.length()>20) {
             editTextRegCognome.setError("Il cognome non può essere vuoto\nMax 20 caratteri");
             editTextRegCognome.requestFocus();
+            return;
+        }
+
+        if(confrontaMail(email)) {
+            editTextRegEmail.setError("L'email è già presente nel sistema");
+            editTextRegEmail.requestFocus();
             return;
         }
 
@@ -159,6 +187,21 @@ public class RegistrazioneActivity extends Activity {
         matcher = pattern.matcher(email);
 
         return matcher.matches();
+    }
+
+    private boolean confrontaMail(String mail) {
+        boolean value= true;
+        for (Utente utente : listaUtente) {
+            if (utente.getEmail().equals(mail)) {
+                value=true;
+                break;
+            }
+            else {
+                value=false;
+            }
+        }
+
+        return value;
     }
 }
 

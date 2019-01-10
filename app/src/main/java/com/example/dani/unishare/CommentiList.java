d4cff5119b191314fc1101d09619dff299744e97
ddevin96 <danieledevinco1996@gmail.com>
2019-01-10 16:43:32 +0100

    format checkstyle login commento e commentolist

--- app/src/main/java/com/example/dani/unishare/CommentiList.java
+++ app/src/main/java/com/example/dani/unishare/CommentiList.java
@@ -10,28 +10,28 @@ import android.widget.TextView;
 import java.util.List;
 
 public class CommentiList extends ArrayAdapter<Commento> {
-    Activity context;
-    List<Commento> lista;
+  Activity context;
+  List<Commento> lista;
 
-    public CommentiList (Activity context, List<Commento> lista) {
-        super(context, R.layout.list_view_commenti, lista);
-        this.context = context;
-        this.lista = lista;
-    }
+  public CommentiList(Activity context, List<Commento> lista) {
+    super(context, R.layout.list_view_commenti, lista);
+    this.context = context;
+    this.lista = lista;
+  }
 
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-        View listViewItem = inflater.inflate(R.layout.list_view_commenti,null, true );
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    LayoutInflater inflater = context.getLayoutInflater();
+    View listViewItem = inflater.inflate(R.layout.list_view_commenti, null, true);
 
-        TextView textViewCommentDescription = (TextView) listViewItem.findViewById(R.id.textViewCommentDescription);
-        TextView textViewCommentAuthor = (TextView) listViewItem.findViewById(R.id.textViewCommentAuthor);
+    TextView textViewCommentDescription = (TextView) listViewItem.findViewById(R.id.textViewCommentDescription);
+    TextView textViewCommentAuthor = (TextView) listViewItem.findViewById(R.id.textViewCommentAuthor);
 
-        Commento commento = lista.get(position);
-        textViewCommentDescription.setText(commento.getDescription());
-        textViewCommentAuthor.setText(commento.getAuthor());
+    Commento commento = lista.get(position);
+    textViewCommentDescription.setText(commento.getDescription());
+    textViewCommentAuthor.setText(commento.getAuthor());
 
-        return listViewItem;
-    }
+    return listViewItem;
+  }
 }
--- app/src/main/java/com/example/dani/unishare/LoginActivity.java
+++ app/src/main/java/com/example/dani/unishare/LoginActivity.java
@@ -29,122 +29,121 @@ import java.util.regex.Pattern;
 
 public class LoginActivity extends Activity {
 
-    EditText email;
-    EditText password;
-    Button accedi;
-    FirebaseAuth databaseLogin;
-    DatabaseReference databaseUtente;
-    List<Utente> listaUtente;
+  EditText email;
+  EditText password;
+  Button accedi;
+  FirebaseAuth databaseLogin;
+  DatabaseReference databaseUtente;
+  List<Utente> listaUtente;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
 
-        email= (EditText) findViewById(R.id.InserisciEmail);
-        password= (EditText) findViewById(R.id.InserisciPassword);
-        accedi= (Button) findViewById(R.id.Accedi);
+    email = (EditText) findViewById(R.id.InserisciEmail);
+    password = (EditText) findViewById(R.id.InserisciPassword);
+    accedi = (Button) findViewById(R.id.Accedi);
 
-        listaUtente = new ArrayList<>();
+    listaUtente = new ArrayList<>();
 
-        databaseLogin = FirebaseAuth.getInstance();
-        databaseUtente = FirebaseDatabase.getInstance().getReference("utente");
+    databaseLogin = FirebaseAuth.getInstance();
+    databaseUtente = FirebaseDatabase.getInstance().getReference("utente");
 
-        databaseUtente.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                listaUtente.clear();
-                for(DataSnapshot utenteSnapshot: dataSnapshot.getChildren()) {
-                    Utente utente = utenteSnapshot.getValue(Utente.class);
-                    listaUtente.add(utente);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-        accedi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String eMail= email.getText().toString();
-                String password1=password.getText().toString();
-
-                if (TextUtils.isEmpty(eMail)||eMail.length()<3||eMail.length()>63||!isValidEmail(eMail)) {
-                    email.setError("L'email non può essere vuota.");
-                    email.requestFocus();
-                    return;
-                }
-
-                if (!confrontaMail(eMail)) {
-                    email.setError("L' e-mail è errata.");
-                    email.requestFocus();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password1)||password1.length()<8||password1.length()>23||!isValidPassword(password1)) {
-                    password.setError("La password non può essere vuota\nMin 8 caratteri\nMax 20 caratteri");
-                    password.requestFocus();
-                    return;
-                }
-
-                databaseLogin.signInWithEmailAndPassword(eMail, password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            finish();
-                            Toast.makeText(getApplicationContext(), "Login effettuato con successo.", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        } else {
-                            Toast.makeText(getApplicationContext(), "L'E-mail o la password sono errate.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
-
-        if(databaseLogin.getCurrentUser()!=null){
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    databaseUtente.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        listaUtente.clear();
+        for (DataSnapshot utenteSnapshot : dataSnapshot.getChildren()) {
+          Utente utente = utenteSnapshot.getValue(Utente.class);
+          listaUtente.add(utente);
         }
-    }
+      }
 
-    private static boolean isValidPassword(String password) {
+      @Override
+      public void onCancelled(@NonNull DatabaseError databaseError) {
 
-        Pattern pattern;
-        Matcher matcher;
-        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.[a-z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
-        pattern = Pattern.compile(PASSWORD_PATTERN);
-        matcher = pattern.matcher(password);
+      }
+    });
 
-        return matcher.matches();
+    accedi.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String eMail = email.getText().toString();
+        String password1 = password.getText().toString();
 
-    }
-
-    private static boolean isValidEmail(String email){
-        Pattern pattern;
-        Matcher matcher;
-        final String EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-        pattern = Pattern.compile(EMAIL_PATTERN);
-        matcher = pattern.matcher(email);
-
-        return matcher.matches();
-    }
-
-    private boolean confrontaMail(String mail) {
-        boolean value= true;
-        for (Utente utente : listaUtente) {
-            if (utente.getEmail().equals(mail)) {
-                value=true;
-                break;
-            }
-            else {
-                value=false;
-            }
+        if (TextUtils.isEmpty(eMail) || eMail.length() < 3 || eMail.length() > 63 || !isValidEmail(eMail)) {
+          email.setError("L'email non può essere vuota.");
+          email.requestFocus();
+          return;
         }
 
-        return value;
+        if (!confrontaMail(eMail)) {
+          email.setError("L' e-mail è errata.");
+          email.requestFocus();
+          return;
+        }
+
+        if (TextUtils.isEmpty(password1) || password1.length() < 8 || password1.length() > 23 || !isValidPassword(password1)) {
+          password.setError("La password non può essere vuota\nMin 8 caratteri\nMax 20 caratteri");
+          password.requestFocus();
+          return;
+        }
+
+        databaseLogin.signInWithEmailAndPassword(eMail, password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+          @Override
+          public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+              finish();
+              Toast.makeText(getApplicationContext(), "Login effettuato con successo.", Toast.LENGTH_SHORT).show();
+              startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            } else {
+              Toast.makeText(getApplicationContext(), "L'E-mail o la password sono errate.", Toast.LENGTH_SHORT).show();
+            }
+          }
+        });
+      }
+    });
+
+    if (databaseLogin.getCurrentUser() != null) {
+      startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
+  }
+
+  private static boolean isValidPassword(String password) {
+
+    Pattern pattern;
+    Matcher matcher;
+    final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.[a-z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+    pattern = Pattern.compile(PASSWORD_PATTERN);
+    matcher = pattern.matcher(password);
+
+    return matcher.matches();
+
+  }
+
+  private static boolean isValidEmail(String email) {
+    Pattern pattern;
+    Matcher matcher;
+    final String EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    pattern = Pattern.compile(EMAIL_PATTERN);
+    matcher = pattern.matcher(email);
+
+    return matcher.matches();
+  }
+
+  private boolean confrontaMail(String mail) {
+    boolean value = true;
+    for (Utente utente : listaUtente) {
+      if (utente.getEmail().equals(mail)) {
+        value = true;
+        break;
+      } else {
+        value = false;
+      }
+    }
+
+    return value;
+  }
 
 }

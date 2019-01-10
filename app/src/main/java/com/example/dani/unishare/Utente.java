f62415acccb5f14c65cedc451ad5b457c1208ae5
ddevin96 <danieledevinco1996@gmail.com>
2019-01-10 15:53:47 +0100

    format checkstyle utente

--- app/src/main/java/com/example/dani/unishare/Utente.java
+++ app/src/main/java/com/example/dani/unishare/Utente.java
@@ -4,91 +4,92 @@ import java.util.Date;
 
 public class Utente {
 
-    private String id;
-    private String nome;
-    private String cognome;
-    private String sesso;
-    private String dataDiNascita;
-    private String email;
-    private String password;
-    private String ruolo;
+  private String id;
+  private String nome;
+  private String cognome;
+  private String sesso;
+  private String dataDiNascita;
+  private String email;
+  private String password;
+  private String ruolo;
 
-    public Utente(){
+  public Utente() {
 
-    }
+  }
 
-    public Utente(String id, String nome, String cognome, String sesso, String dataDiNascita, String email, String password, String ruolo) {
-        this.id = id;
-        this.nome = nome;
-        this.cognome = cognome;
-        this.sesso = sesso;
-        this.dataDiNascita = dataDiNascita;
-        this.email = email;
-        this.password = password;
-        this.ruolo = ruolo;
-    }
+  public Utente(String id, String nome, String cognome, String sesso, String dataDiNascita,
+                String email, String password, String ruolo) {
+    this.id = id;
+    this.nome = nome;
+    this.cognome = cognome;
+    this.sesso = sesso;
+    this.dataDiNascita = dataDiNascita;
+    this.email = email;
+    this.password = password;
+    this.ruolo = ruolo;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public String getCognome() {
-        return cognome;
-    }
+  public String getCognome() {
+    return cognome;
+  }
 
-    public String getSesso() {
-        return sesso;
-    }
+  public String getSesso() {
+    return sesso;
+  }
 
-    public String getDataDiNascita() {
-        return dataDiNascita;
-    }
+  public String getDataDiNascita() {
+    return dataDiNascita;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
+  public void setCognome(String cognome) {
+    this.cognome = cognome;
+  }
 
-    public void setSesso(String sesso) {
-        this.sesso = sesso;
-    }
+  public void setSesso(String sesso) {
+    this.sesso = sesso;
+  }
 
-    public void setDataDiNascita(String dataDiNascita) {
-        this.dataDiNascita = dataDiNascita;
-    }
+  public void setDataDiNascita(String dataDiNascita) {
+    this.dataDiNascita = dataDiNascita;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public String getRuolo() {
-        return ruolo;
-    }
+  public String getRuolo() {
+    return ruolo;
+  }
 
-    public void setRuolo(String ruolo) {
-        this.ruolo = ruolo;
-    }
+  public void setRuolo(String ruolo) {
+    this.ruolo = ruolo;
+  }
 }

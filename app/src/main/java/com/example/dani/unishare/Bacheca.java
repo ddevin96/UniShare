94352d9d641b9baa2854b54928f1f9658bd159e1
ddevin96 <danieledevinco1996@gmail.com>
2019-01-10 16:45:05 +0100

    format checkstyle commenti bachecalist bacheca

--- app/src/main/java/com/example/dani/unishare/Bacheca.java
+++ app/src/main/java/com/example/dani/unishare/Bacheca.java
@@ -3,71 +3,71 @@ package com.example.dani.unishare;
 import java.util.Date;
 
 public class Bacheca {
-    private String id;
-    private String title;
-    private String description;
-    private String author;
-    private String authorId;
-    private Date date;
+  private String id;
+  private String title;
+  private String description;
+  private String author;
+  private String authorId;
+  private Date date;
 
-    public Bacheca() {
+  public Bacheca() {
 
-    }
+  }
 
-    public Bacheca(String id, String title, String description, String author, String authorId, Date data) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.author = author;
-        this.authorId= authorId;
-        this.date = data;
-    }
+  public Bacheca(String id, String title, String description, String author, String authorId, Date data) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.authorId = authorId;
+    this.date = data;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public String getAuthor() {
-        return author;
-    }
+  public String getAuthor() {
+    return author;
+  }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 
-    public void setAuthorId(String authorId) {
-        this.authorId = authorId;
-    }
+  public void setAuthorId(String authorId) {
+    this.authorId = authorId;
+  }
 
-    public String getAuthorId() {
-        return authorId;
-    }
+  public String getAuthorId() {
+    return authorId;
+  }
 
-    public Date getDate() {
-        return date;
-    }
+  public Date getDate() {
+    return date;
+  }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+  public void setDate(Date date) {
+    this.date = date;
+  }
 }
--- app/src/main/java/com/example/dani/unishare/BachecaList.java
+++ app/src/main/java/com/example/dani/unishare/BachecaList.java
@@ -13,25 +13,25 @@ import android.widget.TextView;
 import java.util.List;
 
 public class BachecaList extends ArrayAdapter<Bacheca> {
-    private Activity context;
-    private List<Bacheca> listaBacheca;
+  private Activity context;
+  private List<Bacheca> listaBacheca;
 
-    public BachecaList(Activity context, List<Bacheca> listaBacheca) {
-        super(context, R.layout.list_view_bacheca, listaBacheca);
-        this.context = context;
-        this.listaBacheca = listaBacheca;
-    }
+  public BachecaList(Activity context, List<Bacheca> listaBacheca) {
+    super(context, R.layout.list_view_bacheca, listaBacheca);
+    this.context = context;
+    this.listaBacheca = listaBacheca;
+  }
 
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-        View listViewItem = inflater.inflate(R.layout.list_view_bacheca, null, true);
-        TextView title = (TextView) listViewItem.findViewById((R.id.textViewTitoloBacheca));
+  @NonNull
+  @Override
+  public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    LayoutInflater inflater = context.getLayoutInflater();
+    View listViewItem = inflater.inflate(R.layout.list_view_bacheca, null, true);
+    TextView title = (TextView) listViewItem.findViewById((R.id.textViewTitoloBacheca));
 
-        Bacheca bacheca = listaBacheca.get(position);
-        title.setText(bacheca.getTitle());
+    Bacheca bacheca = listaBacheca.get(position);
+    title.setText(bacheca.getTitle());
 
-        return listViewItem;
-    }
+    return listViewItem;
+  }
 }

0b12fb715cf04ecb90e9a4a797fc973b5cc8ca00
ddevin96 <danieledevinco1996@gmail.com>
2019-01-13 20:32:06 +0100

    forse funziona il test

---
+++ app/src/test/java/com/example/dani/unishare/ManagerActivityTest.java
@@ -0,0 +1,27 @@
+package com.example.dani.unishare;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ManagerActivityTest extends ManagerActivity{
+
+  ManagerActivity act;
+  FirebaseAuth databaseId;
+
+  public void setUp() {
+    act = new ManagerActivity();
+  }
+
+  @Test
+  public void onStart() {
+  }
+
+  @Test
+  public void istance() {
+    if (databaseId != null)
+      assertTrue(true);
+  }
+}

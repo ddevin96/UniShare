f5b93ef30a759ef2296ab7e7ca2ff00f72efd224
Dona9 <dona.9724@gmail.com>
2019-01-15 15:26:03 +0100

    manuale d'istallazione

--- app/src/main/java/com/example/dani/unishare/IntoActivity.java
+++ app/src/main/java/com/example/dani/unishare/IntoActivity.java
@@ -78,7 +78,7 @@ public class IntoActivity extends AppIntro {
         super.onDonePressed(currentFragment);
         // Do something when users tap on Done button.
     }
-
+    //ciao
     @Override
     public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
         super.onSlideChanged(oldFragment, newFragment);
